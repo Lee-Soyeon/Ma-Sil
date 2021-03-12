27c5053173b83e75e4c95380b8f0f234b7f5025f
CHOI <rorigu32@gmail.com>
2021-03-12 17:23:52 +0900

    frontend_default_frame_update

---
+++ app/src/main/java/com/gsc/silverwalk/FinishMissionActivity.kt
@@ -0,0 +1,11 @@
+package com.gsc.silverwalk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinishMissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish_mission)
+    }
+}
--- app/src/main/java/com/gsc/silverwalk/ui/mission/MissionFragment.kt
+++ app/src/main/java/com/gsc/silverwalk/ui/mission/MissionFragment.kt
@@ -1,7 +1,6 @@
 package com.gsc.silverwalk.ui.mission
 
 import android.app.AlertDialog
-import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -9,10 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.gsc.silverwalk.R
 
 class MissionFragment : Fragment() {
@@ -33,7 +29,7 @@ class MissionFragment : Fragment() {
         dialogBuilder = AlertDialog.Builder(context)
         dialogView = layoutInflater.inflate(R.layout.dialog_mission_start, null)
         dialogBuilder.setView(dialogView)
-        dialogButton = dialogView.findViewById<Button>(R.id.dialog_start_ok_button)
+        dialogButton = dialogView.findViewById<Button>(R.id.mission_cancle_dialog_cnacle_button)
         dialogButton.setOnClickListener(View.OnClickListener {
             // Activity Show
 
