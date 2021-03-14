81083fac5c8a7d4ff489ccba7d638e02d4e673ec
CHOI <rorigu32@gmail.com>
2021-03-14 23:12:07 +0900

    firebase

--- app/src/main/java/com/gsc/silverwalk/ui/mission/MissionFragment.kt
+++ app/src/main/java/com/gsc/silverwalk/ui/mission/MissionFragment.kt
@@ -4,37 +4,44 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.gsc.silverwalk.MissionActivity
 import com.gsc.silverwalk.R
-import kotlinx.android.synthetic.main.dialog_mission_cancle.*
 import kotlinx.android.synthetic.main.fragment_mission.*
 
 class MissionFragment : Fragment() {
 
     // Current Mission Parameter
     private var currentMissionIndex = 0
-    private val missionStringArray : Array<Int> = arrayOf(
-        R.string.mission_mission_1, R.string.mission_mission_2,
-        R.string.mission_mission_3, R.string.mission_mission_4,
-        R.string.mission_mission_5
-    )
     private val MissionLevelStringArray : Array<Int> = arrayOf(
         R.string.Easy, R.string.Moderate, R.string.Hard
     )
-    private lateinit var MissionLevelCardView : Array<CardView>
+    private lateinit var missionLevelCardView : Array<CardView>
 
     // Dialog Parameter
     private lateinit var dialogBuilder : AlertDialog.Builder
     private lateinit var dialogView : View
     private lateinit var dialogButton : Button
     private lateinit var dialogObject : AlertDialog
+
+    // Today Mission Data
+    private var missionDataList : MutableList<Map<String,Any>> = mutableListOf()
+    private var backGroundImageURL : List<String> = listOf()
+
+    // UID
+    private val UID = "X5NztOqVUgGPT84WmSiK"
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -50,7 +57,7 @@ class MissionFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         // MissionLevelCardView Init
-        MissionLevelCardView = arrayOf(
+        missionLevelCardView = arrayOf(
             mission_level1_cardview, mission_level2_cardview,
             mission_level3_cardview, mission_level4_cardview,
             mission_level5_cardview
@@ -77,35 +84,89 @@ class MissionFragment : Fragment() {
 
         // prev Mission Button
         mission_select_left_button.setOnClickListener(View.OnClickListener {
-            currentMissionIndex = if (currentMissionIndex == 0) 4 else currentMissionIndex - 1
+            currentMissionIndex =
+                if (currentMissionIndex == 0) (missionDataList.size - 1)
+                else currentMissionIndex - 1
             setMissionParameterByCurrentMissionIndex()
         })
 
         // next Mission Button
         mission_select_right_button.setOnClickListener(View.OnClickListener {
-            currentMissionIndex = if (currentMissionIndex == 4) 0 else currentMissionIndex + 1
+            currentMissionIndex =
+                if (currentMissionIndex == (missionDataList.size - 1)) 0
+                else currentMissionIndex + 1
             setMissionParameterByCurrentMissionIndex()
         })
+
+        findTodayMissionFromFireBase()
+    }
+
+    fun findTodayMissionFromFireBase()
+    {
+        // load Firebase Data
+        val userTodayMissionDocs = Firebase.firestore
+            .collection("users").document(UID)
+            .collection("today_mission").get()
+
+        userTodayMissionDocs
+            .addOnSuccessListener { result ->
+                for(document in result){
+                    missionDataList.add(document.data)
+                }
+
+                setMissionParameterByCurrentMissionIndex()
+            }
+            .addOnFailureListener { exception ->
+                Log.d("FireStore Error", "get failed with ", exception)
+            }
     }
 
     fun setMissionParameterByCurrentMissionIndex(){
 
-        mission_number_text.setText(missionStringArray[currentMissionIndex])
+        // Set mission string
+        var mission_string =
+            getString(R.string.mission_display_1) +
+            getString(R.string.mission_display_2) +
+            getString(R.string.mission_display_3)
+        mission_string = mission_string.replace("(%location)",
+            missionDataList[currentMissionIndex].get("location").toString())
+        mission_string = mission_string.replace("(%time)",
+            ((missionDataList[currentMissionIndex].get("time") as Long) / 60).toString())
+        mission_string = mission_string.replace("(%type)",
+            missionDataList[currentMissionIndex].get("type").toString())
 
-        /*
-        // Mission Explain Text Update
-        mission_display_1_text.setText()
-        mission_display_2_text.setText()
-        mission_display_3_text.setText()
+        // Update Display Text
+        val splitString = mission_string.split("(%tab)")
+        mission_display_1_text.setText(splitString[0])
+        mission_display_2_text.setText(splitString[1])
+        mission_display_3_text.setText(splitString[2])
 
-        // Mission Level Update
-        mission_level_text.setText(MissionLevelStringArray[MissionLevel])
-        for(i in 0..MissionLevel){
-            MissionLevelCardView[i].setColor()
-        }
+        mission_level_text.setText(MissionLevelStringArray[
+                (missionDataList[currentMissionIndex].get("level") as Long).toInt()])
 
-        // Background Image Update
-        mission_background.setImageURI()
-         */
+        // Set BackGround Image
+        Firebase.firestore.collection("location")
+            .whereEqualTo("name",
+                missionDataList[currentMissionIndex].get("location").toString())
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if(!querySnapshot.isEmpty){
+                    backGroundImageURL =
+                        querySnapshot.documents.get(0).data?.get("image_paths") as List<String>
+
+                    FirebaseStorage.getInstance().getReferenceFromUrl(backGroundImageURL[0])
+                        .downloadUrl.addOnSuccessListener { uri ->
+                            Glide.with(this).load(uri).into(mission_background)
+                        }
+                        .addOnFailureListener { exception ->
+                            Log.d("Fire Storage Failed","get failed with ", exception)
+                        }
+                }else{
+                    // mission_background.setImageURI("DefaultImageURL")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("FireStore Failed", "get failed with ", exception)
+            }
     }
 }
